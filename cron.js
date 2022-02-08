06f47f0cf28efb2806be39cb71e5b1e587d05cd3
Estelle Comment <estelle.comment@gmail.com>
2022-02-08 17:55:17 +0100

    Clean up script

--- cron.js
+++ cron.js
@@ -1,26 +1,6 @@
-//const cron = require("cron")
-
-/*const job =
-  {
-    cronTime: "* * * * *", // every minute
-    onTick: fetchFromS3,
-    start: true,
-    timeZone: "Europe/Paris",
-    isActive: true,
-    name: "Fetch from s3",
-  }
-
-if (job.isActive) {
-  console.log(`🚀 The job "${job.name}" is ON`)
-  new cron.CronJob(job)
-  console.log(`Started job ${job.name}`)
-} else {
-  console.log(`❌ The job "${job.name}" is OFF`)
-}
-*/
 
 const fetchFromS3 = () => {
-  console.log("Coucou !!!!!")
+  console.log("Fetching from S3")
 }
 
 fetchFromS3()
