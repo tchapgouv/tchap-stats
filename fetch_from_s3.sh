b79b9e04971e6761ce22e1335dab9c9bda94bf8c
Estelle Comment <estelle.comment@gmail.com>
2022-02-09 11:05:12 +0100

    Working fetch script !

--- fetch_from_s3.sh
+++ fetch_from_s3.sh
@@ -3,6 +3,7 @@
 # Fetch the csv file of stats from S3 bucket.
 
 file_to_download=$1
+echo "file to download : $file_to_download"
 
 filename=${file_to_download##*/}
 
@@ -12,19 +13,17 @@ s3path="/${bucket}/${file_to_download}"
 # metadata
 contentType="text/csv"
 dateValue=`date -R`
-signature_string="PUT\n\n${contentType}\n${dateValue}\n${s3path}"
+signature_string="GET\n\n${contentType}\n${dateValue}\n${s3path}"
 
-# TODO get S3SECRET and S3ACCESS
 #prepare signature hash to be sent in Authorization header
-signature_hash=`echo -en ${signature_string} | openssl sha1 -hmac ${S3SECRET} -binary | base64`
+signature_hash=`echo -en ${signature_string} | openssl sha1 -hmac ${S3_SECRET_ACCESS_KEY} -binary | base64`
 
 destination="https://tchap-stats-metabase.s3.gra.cloud.ovh.net/${filename}"
 echo $destination
 
-# TODO use GET here
-# actual curl command to do PUT operation on s3
-curl -k -X PUT -T "${file_to_download}" \
+# actual curl command to do GET operation on s3
+curl -k -X GET -o "${file_to_download}"\
   -H "Date: ${dateValue}" \
   -H "Content-Type: ${contentType}" \
-  -H "Authorization: AWS ${S3ACCESS}:${signature_hash}" \
+  -H "Authorization: AWS ${S3_ACCESS_KEY_ID}:${signature_hash}" \
   $destination
