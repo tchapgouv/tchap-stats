a1751e43fc264d80d764cb6ed1ae5d777314eff7
Calev Eliacheff <c.eliacheff@gmail.com>
2022-02-10 11:22:22 +0100

    insert instead of merge

--- tables.sql
+++ tables.sql
@@ -1,1 +1,2 @@
 CREATE TABLE IF NOT EXISTS subscriptions_aggregate (subscriptions INTEGER, domain VARCHAR, hour timestamp  with time zone, instance VARCHAR);
+-- CREATE UNIQUE INDEX ON subscriptions_aggregate (domain, hour, instance)
