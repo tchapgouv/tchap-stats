4f4b7d6fa6bba3628bb65907f80dcc79b974ea02
Raphaël ALLOUCHE <r.allouche@protonmail.com>
2022-02-11 14:16:00 +0100

    added events table

--- tables.sql
+++ tables.sql
@@ -1,2 +1,18 @@
-CREATE TABLE IF NOT EXISTS subscriptions_aggregate (subscriptions INTEGER, domain VARCHAR, hour timestamp with time zone, instance VARCHAR);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_stats ON subscriptions_aggregate (domain, hour, instance)
+CREATE TABLE IF NOT EXISTS subscriptions_aggregate (
+  subscriptions INTEGER NOT NULL,
+  domain VARCHAR NOT NULL,
+  hour timestamp with time zone NOT NULL,
+  instance VARCHAR NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_stats ON subscriptions_aggregate (domain, hour, instance);
+
+CREATE TABLE IF NOT EXISTS events_aggregate (
+  events INTEGER NOT NULL,
+  type VARCHAR NOT NULL,
+  domain VARCHAR NOT NULL,
+  hour timestamp with time zone NOT NULL,
+  instance VARCHAR NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_stats ON subscriptions_aggregate (domain, hour, instance, type);
