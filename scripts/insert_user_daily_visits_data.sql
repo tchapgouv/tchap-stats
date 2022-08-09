0956c0d8f9433d09b6b8e7ff1460d49c5d495d8a
mcalinghee <mcalinghee.dev@gmail.com>
2022-08-09 16:41:12 +0200

    correct filename

--- scripts/insert_user_daily_visits_data.sql
+++ scripts/insert_user_daily_visits_data.sql
@@ -2,7 +2,7 @@
 
 CREATE TEMPORARY TABLE user_daily_visits_aggregate_temp (visits INTEGER, domain VARCHAR, instance VARCHAR, visit_ts timestamp with time zone);
 
-\copy user_daily_visits_aggregate_temp(visits, visit_ts, domain, instance) FROM '/app/user_daily_visits_aggregate_temp.csv' DELIMITER ',' CSV HEADER;
+\copy user_daily_visits_aggregate_temp(visits, visit_ts, domain, instance) FROM '/app/user_daily_visits_aggregate.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO user_daily_visits_aggregate
 SELECT *
