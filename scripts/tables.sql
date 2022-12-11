ee00e7c102c70e3dd22cf878f2b30120b11c6e5a
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2022-12-11 22:21:56 +0100

    Update tables.sql

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -43,20 +43,28 @@ user_id,
 instance,
 domain,
 CASE
-WHEN user_agent LIKE 'Mozilla%Mac OS%Safari%' THEN 'Safari Mac OS'
 WHEN user_agent LIKE 'Mozilla%Windows%Firefox%' THEN 'Firefox Windows'
 WHEN user_agent LIKE 'Mozilla%Windows%Chrome%' THEN 'Chrome Windows'
+WHEN user_agent LIKE 'Mozilla%Windows%Trident%' THEN 'Internet Explorer Windows'
 WHEN user_agent LIKE 'Mozilla%Mac OS%Firefox%' THEN 'Firefox Mac OS'
 WHEN user_agent LIKE 'Mozilla%Mac OS%Chrome%' THEN 'Chrome Mac OS'
+WHEN user_agent LIKE 'Mozilla%Mac OS%Safari%' THEN 'Safari Mac OS'
 WHEN user_agent LIKE 'Mozilla%Linux%Firefox%' THEN 'Firefox Linux'
 WHEN user_agent LIKE 'Mozilla%Linux%Chrome%' THEN 'Chrome Linux'
+WHEN user_agent LIKE 'Mozilla%Android%Firefox%' THEN 'Firefox Android'
+WHEN user_agent LIKE 'Mozilla%Android%Chrome%' THEN 'Chrome Android'
+WHEN user_agent LIKE 'Mozilla%CrOS%Chrome%' THEN 'Chrome OS'
+WHEN user_agent LIKE 'Mozilla%Mobile%' THEN 'Navigateur Mobile'
+WHEN user_agent LIKE 'Mozilla%' THEN 'Autre Navigateur'
 WHEN user_agent LIKE 'Tchap%NEO%' THEN 'Tchap Android NEO'
 WHEN user_agent LIKE 'Tchap%Android%' THEN 'Tchap Android'
-WHEN user_agent LIKE 'Riot%/2%iOS%' THEN 'Tchap iOS'
+WHEN user_agent LIKE 'RiotNSE/2%iOS%' THEN 'Tchap iOS'
+WHEN user_agent LIKE 'RiotSharedExtension/2%iOS%' THEN 'Tchap iOS'
 WHEN user_agent LIKE 'Tchap%iOS%' THEN 'Tchap iOS'
 WHEN user_agent LIKE 'Riot%' THEN 'Element'
 WHEN user_agent LIKE 'Element%' THEN 'Element'
 ELSE 'Autre'
 END as device_type
 FROM user_daily_visits
+WHERE user_agent != 'matrix-media-repo'
 GROUP BY device_id, month, user_id, instance, domain, device_type;
