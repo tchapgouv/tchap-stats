fb3a4022388fb14128a13cc9aebb7322efcc93d3
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2023-04-15 02:55:59 +0200

    Update scripts/tables.sql

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -86,22 +86,75 @@ CREATE UNIQUE INDEX user_monthly_visits_index ON user_monthly_visits (month,devi
 CREATE INDEX; # Needed for view update
 
 
+CREATE MATERIALIZED VIEW daily_unique_user_count AS
+SELECT
+  date_trunc('day', visit_ts) AS day,
+  user_id,
+  instance,
+  domain,
+  COUNT(*) AS visits_count,
+  COUNT(CASE
+    WHEN user_agent LIKE 'Mozilla%' THEN 1
+    ELSE NULL
+  END) AS web_visits_count,
+  COUNT(CASE
+    WHEN user_agent LIKE 'Tchap%Android%' OR
+         user_agent LIKE 'RiotNSE/2%iOS%' OR
+         user_agent LIKE 'RiotSharedExtension/2%iOS%' OR
+         user_agent LIKE 'Tchap%iOS%' OR
+         user_agent LIKE 'Riot%iOS' OR
+         user_agent LIKE 'Riot%Android' OR
+         user_agent LIKE 'Element%Android' OR
+         user_agent LIKE 'Element%iOS'
+    THEN 1
+    ELSE NULL
+  END) AS mobile_visits_count,
+  COUNT(CASE
+    WHEN user_agent NOT LIKE 'Mozilla%' AND
+         user_agent NOT LIKE 'Tchap%Android%' AND
+         user_agent NOT LIKE 'RiotNSE/2%iOS%' AND
+         user_agent NOT LIKE 'RiotSharedExtension/2%iOS%' AND
+         user_agent NOT LIKE 'Tchap%iOS%' AND
+         user_agent NOT LIKE 'Riot%iOS' AND
+         user_agent NOT LIKE 'Riot%Android' AND
+         user_agent NOT LIKE 'Element%Android' AND
+         user_agent NOT LIKE 'Element%iOS'
+    THEN 1
+    ELSE NULL
+  END) AS other_visits_count
+FROM
+  user_daily_visits
+GROUP BY
+  date_trunc('day', visit_ts),
+  user_id,
+  instance,
+  domain;
+
 CREATE MATERIALIZED VIEW unique_user_daily_count_30d AS
 WITH date_range AS (
   SELECT generate_series(
-    CURRENT_DATE - INTERVAL '3 months',
+    CURRENT_DATE - INTERVAL '30 days',
     CURRENT_DATE,
     INTERVAL '1 day'
   )::date AS day
 ),
+filtered_daily_unique_user_count AS (
+  SELECT
+    day,
+    user_id
+  FROM
+    daily_unique_user_count
+  WHERE
+    day > CURRENT_DATE - INTERVAL '60 days' AND
+    NOT (mobile_visits_count = 0 AND web_visits_count = 0 AND other_visits_count > 0)
+),
 unique_users_per_day AS (
   SELECT
     date_range.day,
-    COUNT(DISTINCT user_daily_visits.user_id) AS unique_user_count
+    COUNT(DISTINCT filtered_daily_unique_user_count.user_id) AS unique_user_count
   FROM
     date_range
-    LEFT JOIN user_daily_visits ON user_daily_visits.visit_ts >= date_range.day - INTERVAL '30 days'
-    AND user_daily_visits.visit_ts < date_range.day + INTERVAL '1 day'
+    LEFT JOIN filtered_daily_unique_user_count ON filtered_daily_unique_user_count.day BETWEEN date_range.day - INTERVAL '30 days' AND date_range.day - INTERVAL '1 day'
   GROUP BY
     date_range.day
 )
