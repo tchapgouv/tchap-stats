1ed9ed2534b9382e4bb524a7275f50bda7a4a1e5
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2023-05-04 22:22:55 +0200

    Separate Refresh Materialized View on a different file

---
+++ scripts/refresh_materialized_view.sql
@@ -0,0 +1,4 @@
+/* Refresh Materialized View (after data are inserted) */
+REFRESH MATERIALIZED VIEW CONCURRENTLY user_monthly_visits;
+REFRESH MATERIALIZED VIEW daily_unique_user_count;
+REFRESH MATERIALIZED VIEW unique_user_daily_count_30d;
