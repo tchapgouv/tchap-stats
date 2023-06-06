8c4211cc480c409aafcce33e043c02e64cdb6757
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2023-06-06 16:53:00 +0200

    Update refresh_materialized_view.sql

--- scripts/refresh_materialized_view.sql
+++ scripts/refresh_materialized_view.sql
@@ -2,3 +2,4 @@
 REFRESH MATERIALIZED VIEW CONCURRENTLY user_monthly_visits;
 REFRESH MATERIALIZED VIEW daily_unique_user_count;
 REFRESH MATERIALIZED VIEW unique_user_daily_count_30d;
+REFRESH MATERIALIZED VIEW month_unique_user_count;
