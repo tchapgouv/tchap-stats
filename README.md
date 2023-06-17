27e52fab58b1bd6e8c3c00ca7fed5f4f0a9792cc
Olivier D <odelcroi@gmail.com>
2023-06-17 16:03:17 +0200

    Fix materialized view script by creating time limited view (#36)
    * ajout view materialisé 30d, 120d, 1y
    * add monthly visit

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ See doc in https://doc.scalingo.com/platform/app/web-less-app
 
 You may need to first create a dummy app to get the scalingo machine up (see [this commit](https://github.com/tchapgouv/tchap-stats/commit/ad9ab080922d8150e69dc224b87562898038f6b8)), then scale the web container to zero, then remove the dummy app.
 
+Access the database
+
+add your public ssh key to your scalingo profile
+with a tunnel 
+
+scalingo -a tchap-stats-prod db-tunnel SCALINGO_POSTGRESQL_URL
+You can access your database on:
+127.0.0.1:10000
+
+
+## Create a local tunnel 
+scalingo -a tchap-stats-prod db-tunnel --region osc-secnum-fr1 SCALINGO_POSTGRESQL_URL
+
+## Connect to the machine
+scalingo --region osc-secnum-fr1 -a tchap-stats-prod run bash 
---
+++ scripts/user_daily_visits_by_month_1y.sql
@@ -0,0 +1,61 @@
+/*
+   vue matérialisée qui agrege les données de user_daily_visits par device et par  utilisateur et par mois
+*/
+DROP IF EXISTS MATERIALIZED VIEW user_daily_visits_by_month_1y; 
+
+/** User Monthly Visits **/
+CREATE MATERIALIZED VIEW user_daily_visits_by_month_1y AS
+SELECT device_id,
+date_trunc('month', visit_ts) as month,
+user_id,
+instance,
+domain,
+CASE
+WHEN user_agent LIKE 'Mozilla%Windows%Firefox%' THEN 'Firefox Windows'
+WHEN user_agent LIKE 'Mozilla%Windows%Chrome%' THEN 'Chrome Windows'
+WHEN user_agent LIKE 'Mozilla%Windows%Trident%' THEN 'Internet Explorer Windows'
+WHEN user_agent LIKE 'Mozilla%Mac OS%Firefox%' THEN 'Firefox Mac OS'
+WHEN user_agent LIKE 'Mozilla%Mac OS%Chrome%' THEN 'Chrome Mac OS'
+WHEN user_agent LIKE 'Mozilla%Mac OS%Safari%' THEN 'Safari Mac OS'
+WHEN user_agent LIKE 'Mozilla%Android%Firefox%' THEN 'Firefox Android'
+WHEN user_agent LIKE 'Mozilla%Android%Chrome%' THEN 'Chrome Android'
+WHEN user_agent LIKE 'Mozilla%Linux%Firefox%' THEN 'Firefox Linux'
+WHEN user_agent LIKE 'Mozilla%Linux%Chrome%' THEN 'Chrome Linux'
+WHEN user_agent LIKE 'Mozilla%CrOS%Chrome%' THEN 'Chrome OS'
+WHEN user_agent LIKE 'Mozilla%Mobile%' THEN 'Navigateur Mobile'
+WHEN user_agent LIKE 'Mozilla%' THEN 'Autre Navigateur'
+WHEN user_agent LIKE 'Tchap%NEO%' THEN 'Tchap Android NEO'
+WHEN user_agent LIKE 'Tchap%Android%' THEN 'Tchap Android'
+WHEN user_agent LIKE 'RiotNSE/2%iOS%' THEN 'Tchap iOS'
+WHEN user_agent LIKE 'RiotSharedExtension/2%iOS%' THEN 'Tchap iOS'
+WHEN user_agent LIKE 'Tchap%iOS%' THEN 'Tchap iOS'
+WHEN user_agent LIKE 'Riot%' THEN 'Element'
+WHEN user_agent LIKE 'Element%' THEN 'Element'
+ELSE 'Autre'
+END as device_type,
+CASE
+WHEN user_agent LIKE 'Mozilla%' THEN 'Web'
+WHEN user_agent LIKE 'Tchap%Android%' THEN 'Mobile'
+WHEN user_agent LIKE 'RiotNSE/2%iOS%' THEN 'Mobile'
+WHEN user_agent LIKE 'RiotSharedExtension/2%iOS%' THEN 'Mobile'
+WHEN user_agent LIKE 'Tchap%iOS%' THEN 'Mobile'
+WHEN user_agent LIKE 'Riot%iOS' THEN 'Mobile'
+WHEN user_agent LIKE 'Riot%Android' THEN 'Mobile'
+WHEN user_agent LIKE 'Element%Android' THEN 'Mobile'
+WHEN user_agent LIKE 'Element%iOS' THEN 'Mobile'
+ELSE 'Autre'
+END as platform
+FROM user_daily_visits
+WHERE user_agent != 'matrix-media-repo'  /** We remove matrix-media-repo they are duplicate between Tchap iOS and Android **/
+  AND visit_ts >= NOW() - INTERVAL '1 year' /* only one year */ 
+GROUP BY 
+  device_id, 
+  month, 
+  user_id, 
+  instance, 
+  domain, 
+  device_type, 
+  platform;
+
+CREATE INDEX IF NOT EXISTS idx_month ON user_monthly_visits (month);
+CREATE UNIQUE INDEX IF NOT EXISTS user_monthly_visits_index ON user_monthly_visits (month,device_id,user_id,device_type);
