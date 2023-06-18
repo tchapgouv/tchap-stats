18c50f15e6e1b892f6e9b78bf74c4e2889db62db
olivier <odelcroi@gmail.com>
2023-06-18 20:14:19 +0200

    fix drop if

--- scripts/user_daily_visits_by_month_1y.sql
+++ scripts/user_daily_visits_by_month_1y.sql
@@ -1,7 +1,7 @@
 /*
    vue matérialisée qui agrege les données de user_daily_visits par device et par  utilisateur et par mois
 */
-DROP IF EXISTS MATERIALIZED VIEW user_daily_visits_by_month_1y; 
+DROP MATERIALIZED VIEW IF EXISTS user_daily_visits_by_month_1y; 
 
 /** User Monthly Visits **/
 CREATE MATERIALIZED VIEW user_daily_visits_by_month_1y AS
