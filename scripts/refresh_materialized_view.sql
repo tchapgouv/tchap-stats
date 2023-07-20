98e2ce709c26b4cf52aa4f1e72298d6620525111
olivier <odelcroi@gmail.com>
2023-07-20 09:54:55 +0200

    reactivate user_monthly_visits

--- scripts/refresh_materialized_view.sql
+++ scripts/refresh_materialized_view.sql
@@ -1,4 +1,4 @@
 /* Refresh Materialized View (after data are inserted) */
-/*REFRESH MATERIALIZED VIEW CONCURRENTLY user_monthly_visits; moved to user_daily_visits_by_month_1y.sql */ 
+REFRESH MATERIALIZED VIEW CONCURRENTLY user_monthly_visits; /* DEPRECATED use instead : user_daily_visits_by_month_1y.sql */ 
 REFRESH MATERIALIZED VIEW daily_unique_user_count; /*trop lourde ne fonctionne pas*/
 REFRESH MATERIALIZED VIEW unique_user_daily_count_30d; /* depend de daily_unique_user_count*/
