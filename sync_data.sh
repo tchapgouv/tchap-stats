3d3463fa189755bc9df3d9ba8e79488aefa7d87e
Olivier D <odelcroi@gmail.com>
2023-10-11 10:54:26 +0200

    Update sync_data.sh

--- sync_data.sh
+++ sync_data.sh
@@ -21,9 +21,10 @@ echo "Fetch S3 subscriptions_aggregate"
 time ./fetch_from_s3.sh subscriptions_aggregate $extract_date
 echo "Fetch S3 events_aggregate"
 time ./fetch_from_s3.sh events_aggregate $extract_date
-echo "Fetch S3 time ./fetch_from_s3.sh user_daily_visits $extract_date
-"
+echo "Fetch S3 user_daily_visits $extract_date"
 time ./fetch_from_s3.sh user_daily_visits $extract_date
+echo "Fetch S3 pushers $extract_date"
+time ./fetch_from_s3.sh pushers $extract_date
 
 ## Set up DB
 psql -d $DATABASE_URL -f scripts/tables.sql
