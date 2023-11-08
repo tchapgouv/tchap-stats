b3c253c8594b35957d3316d5f2c1da088b77b889
olivier <odelcroi@gmail.com>
2023-11-08 12:18:40 +0100

    fix copy

--- scripts/insert_account_data_data.sql
+++ scripts/insert_account_data_data.sql
@@ -5,7 +5,7 @@ CREATE TEMPORARY TABLE account_data_aggregate_temp (user_id VARCHAR, account_dat
 
 -- CSV file has fields in this order : user_id	account_data_type	content	instance domain
 -- If it changes, change this line or it will break.
-\copy account_data_aggregate_temp(user_id VARCHAR, account_data_type VARCHAR, content VARCHAR, instance VARCHAR, domain VARCHAR) FROM '/app/account_data.csv' DELIMITER ',' CSV HEADER;
+\copy account_data_aggregate_temp(user_id, account_data_type, content, instance, domain) FROM '/app/account_data.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO account_data_aggregate
 SELECT *
