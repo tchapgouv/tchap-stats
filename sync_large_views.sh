a9691f2d22748c8d7348962eadb1f222323a42dd
olivier <odelcroi@gmail.com>
2024-10-16 13:56:50 +0200

    deactivate others outdated views

--- sync_large_views.sh
+++ sync_large_views.sh
@@ -20,8 +20,8 @@ time psql -d $DATABASE_URL -f scripts/user_daily_visits_by_month_1y.sql
 #echo "Recreate Materialized View : user_daily_visits_by_month_18m"
 #time psql -d $DATABASE_URL -f scripts/user_daily_visits_by_month_18m.sql
 
-echo "Refresh Materialized View"
-time psql -d $DATABASE_URL -f scripts/refresh_materialized_view.sql
+#echo "Refresh Materialized View"
+#time psql -d $DATABASE_URL -f scripts/refresh_materialized_view.sql
 
 echo "Done !"
 
