d02423d5728de10b252c5338e0327f1e8dec6b36
olivier <odelcroi@gmail.com>
2024-10-29 11:22:10 +0100

    fix force condition

--- start_cron.py
+++ start_cron.py
@@ -8,18 +8,20 @@ def start_cron():
     # Essayez d'obtenir la valeur de la variable d'environnement 'CRON_SCHEDULE'
     # Si elle n'existe pas, utilisez une valeur par défaut ('01:00' par exemple) UTC
     cron_schedule:str = os.getenv('CRON_SCHEDULE', '01:00')
-    force_sync_at_start:bool = os.getenv('FORCE_SYNC_AT_START', False)
+    force_sync_at_start = os.getenv('FORCE_SYNC_AT_START', 'False').lower() == 'true'
 
     print(f"FORCE_SYNC_AT_START : {force_sync_at_start}")
 
 
     #if wanted, launch the sync stats process when container is restarted
     if force_sync_at_start:
+        print("Forcing synchronization at start...")
         try: 
             job_sync_stats()
         except:
             print(f"job_sync_stats job has failed")
-
+    else:
+        print("Skipping synchronization at start.")
 
     print(f"CRON SCHEDULE: {cron_schedule}")
     schedule.every().day.at(cron_schedule).do(job_sync_stats)    
