e7d7e30c9a536b1b29dbb60549d20e96916a1c06
olivier <odelcroi@gmail.com>
2024-11-05 09:54:20 +0100

    test without timezone

--- scripts/insert_user_daily_visits_data.sql
+++ scripts/insert_user_daily_visits_data.sql
@@ -1,13 +1,13 @@
 /* Insert data from the csv file into the DB. Some of the data will be already present in the DB, they will be ignored. */
 
-CREATE TEMPORARY TABLE user_daily_visits_temp (user_id VARCHAR, device_id VARCHAR, visit_ts timestamp with time zone, user_agent VARCHAR, instance VARCHAR, domain VARCHAR);
+CREATE TEMPORARY TABLE user_daily_visits_temp (user_id VARCHAR, device_id VARCHAR, visit_ts date, user_agent VARCHAR, instance VARCHAR, domain VARCHAR);
 
 \COPY user_daily_visits_temp(user_id, device_id, visit_ts, user_agent, instance, domain) FROM '/app/user_daily_visits.csv' DELIMITER ',' CSV HEADER;
 
-INSERT INTO user_daily_visits
-SELECT *
+INSERT INTO user_daily_visits(user_id, device_id, visit_ts, user_agent, instance, domain, visit_ts)
+SELECT (user_id, device_id, visit_ts, user_agent, instance, domain, added_date) -- new column added_date converted from visit_ts
 FROM user_daily_visits_temp udvt
 WHERE 
-  udvt.visit_ts >= NOW() - INTERVAL '3 days' -- insert only 3 days of history to speed up
+  udvt.visit_ts >= NOW() - INTERVAL '30 days' -- insert only 3 days of history to speed up
 ON CONFLICT DO NOTHING
 
