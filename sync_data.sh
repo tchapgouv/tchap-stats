ec27bfdde0c68d5476696c85fc24e8d383d421bf
olivier <odelcroi@gmail.com>
2024-11-14 12:04:19 +0100

    reorganize by pipeline

--- sync_data.sh
+++ sync_data.sh
@@ -17,40 +17,54 @@ fi
 
 echo $extract_date
 
+# Set up DB - create table and index
+psql -d $DATABASE_URL -f scripts/tables.sql
+
+# Retrieve and insert data
+
+## USER DAILY VISITS
+echo "Fetch S3 user_daily_visits $extract_date"
+time ./fetch_from_s3.sh user_daily_visits $extract_date
+echo "Insert User Daily Visits"
+time psql -d $DATABASE_URL -f scripts/insert_user_daily_visits_data.sql
+
+## SUBSCRIPTIONS
 echo "Fetch S3 subscriptions_aggregate"
 time ./fetch_from_s3.sh subscriptions_aggregate $extract_date
+echo "Insert Subscriptions"
+time psql -d $DATABASE_URL -f scripts/insert_subscriptions_data.sql
+
+## EVENTS
 #echo "Fetch S3 events_aggregate"
 # time ./fetch_from_s3.sh events_aggregate $extract_date
 echo "Fetch S3 events_roomv9_aggregate"
 time ./fetch_from_s3.sh events_roomv9_aggregate $extract_date
-echo "Fetch S3 user_daily_visits $extract_date"
-time ./fetch_from_s3.sh user_daily_visits $extract_date
-echo "Fetch S3 pushers $extract_date"
-time ./fetch_from_s3.sh pushers $extract_date
-echo "Fetch S3 account_data $extract_date"
-time ./fetch_from_s3.sh account_data $extract_date
-echo "Fetch S3 crisp_conversation_segments $extract_date"
-time ./fetch_from_s3.sh crisp_conversation_segments $extract_date
-
-
-## Set up DB
-psql -d $DATABASE_URL -f scripts/tables.sql
-
-#### Now insert into DB
-echo "Insert Subscriptions"
-time psql -d $DATABASE_URL -f scripts/insert_subscriptions_data.sql
-# echo "Insert Events"
-# time psql -d $DATABASE_URL -f scripts/insert_events_data.sql
 echo "Insert Events roomv9"
 time psql -d $DATABASE_URL -f scripts/insert_events_roomv9_data.sql
-echo "Insert User Daily Visits"
-time psql -d $DATABASE_URL -f scripts/insert_user_daily_visits_data.sql
+
+## PUSHERS
+echo "Fetch S3 pushers $extract_date"
+time ./fetch_from_s3.sh pushers $extract_date
 echo "Insert Pushers"
 time psql -d $DATABASE_URL -f scripts/insert_pushers_data.sql
+
+
+## ACCOUNT DATA
+echo "Fetch S3 account_data $extract_date"
+time ./fetch_from_s3.sh account_data $extract_date
 echo "Insert Account Data"
 time psql -d $DATABASE_URL -f scripts/insert_account_data_data.sql
-echo "Insert crisp conversation segments"
+
+## CRISP
+echo "Fetch S3 crisp_conversation_segments $extract_date"
+time ./fetch_from_s3.sh crisp_conversation_segments $extract_date
+echo "Insert crisp conversations segments"
 time psql -d $DATABASE_URL -f scripts/insert_crisp_conversation_segments.sql
+
+
+
+
+
 
 echo "Done !"
 
