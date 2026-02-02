a632f50d628377938e8913ea21718d4013115eb3
Olivier D <odelcroi@gmail.com>
2026-02-02 13:32:11 +0100

    Update insert_sso_data.sql
    doc

--- scripts/insert_sso_data.sql
+++ scripts/insert_sso_data.sql
@@ -1,5 +1,6 @@
 /* Insert data from the csv file into the DB. */
 /* total_users	domain	instance	users_legacy	users_both_authentication	users_proconnect */
+/* import workflow : https://github.com/tchapgouv/tchap-infra/blob/master/ansible/roles/matrix-synapse-stats/files/export_sso_stats */
 
 CREATE TEMPORARY TABLE sso_aggregate_temp (
 total_users INTEGER,
